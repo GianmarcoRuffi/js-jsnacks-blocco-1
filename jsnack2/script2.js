4ac260aaa112bfa5d4a92653bbfd02b41ae3bf62
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-03-31 18:08:45 +0200

    Jsnack 2

--- jsnack2/script2.js
+++ jsnack2/script2.js
@@ -4,21 +4,24 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.*/
 
 let numList = [];
-numList.length = 6;
+
+let questionCount = 0;
 
 do {
   let enteredNumber = parseInt(prompt("Please enter a number "));
-  if (number % 2 == 0) {
-    console.log("The number is even.");
+  if (enteredNumber % 2 == 0) {
+    console.log("The number is Even. It won't be added.");
+    questionCount += 1;
   }
 
   // if the number is odd
   else {
     numList.push(enteredNumber);
-    console.log("The number is odd.");
+    questionCount += 1;
+    console.log("The number is Odd. Added to the array.");
     console.log(numList);
   }
-} while (isNaN(enteredNumber) || enteredNumber < 6);
+} while (questionCount < 6);
 
-console.log(number);
 console.log(numList);
+console.log("Array length: " + numList.length);
